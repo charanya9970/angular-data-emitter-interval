1266f902890e292275a6f6d3501767f0c66e8438
charanya9970 <charanya9970@gmail.com>
2021-02-16 09:50:43 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-data-emitter-interval
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-data-emitter-interval)
---
+++ src/app/data-emitter/data-emitter.component.ts
@@ -0,0 +1,47 @@
+import { Component, Input, OnInit, Output } from "@angular/core";
+import { interval } from "rxjs";
+import { flatMap, map } from "rxjs/operators";
+import { Http } from "@angular/http";
+
+@Component({
+  selector: 'app-data-emitter',
+  templateUrl: './data-emitter.component.html',
+  styleUrls: ['./data-emitter.component.css']
+})
+export class DataEmitterComponent implements OnInit {
+
+
+
+  @Output() data: any;
+  @Input() apiUrl: any;
+  @Input() intervalPeriod: number;
+
+  minutes: number = 10000;
+  subscription$: any;
+
+  constructor(private http: Http) {}
+  ngOnInit() {
+    this.getData().subscribe(data => {
+        this.data = data;
+        console.log(this.data);
+      });
+
+    this.minutes = this.intervalPeriod * 60 * 1000;
+    this.subscription$ = interval(this.minutes)
+      .pipe(flatMap(() => this.getData()))
+      .subscribe(data => {
+        this.data = data;
+        console.log(this.data);
+      });
+  }
+
+  getData() {
+    return this.http
+      .get(this.apiUrl)
+      .pipe(map((response: any) => response.json()));
+  }
+
+  ngOnDestroy() {
+    this.subscription$.unsubscribe();
+  }
+}
