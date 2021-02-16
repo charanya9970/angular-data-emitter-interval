e6e727b492879d4661be1d949b3b6311ccadc626
charanya9970 <charanya9970@gmail.com>
2021-02-16 10:08:33 -0500

    1 file modified

--- src/app/data-emitter/data-emitter.component.ts
+++ src/app/data-emitter/data-emitter.component.ts
@@ -2,16 +2,14 @@ import { Component, Input, OnInit, Output } from "@angular/core";
 import { interval } from "rxjs";
 import { flatMap, map } from "rxjs/operators";
 import { Http } from "@angular/http";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-data-emitter',
-  templateUrl: './data-emitter.component.html',
-  styleUrls: ['./data-emitter.component.css']
+  selector: "app-data-emitter",
+  templateUrl: "./data-emitter.component.html",
+  styleUrls: ["./data-emitter.component.css"]
 })
 export class DataEmitterComponent implements OnInit {
-
-
-
   @Output() data: any;
   @Input() apiUrl: any;
   @Input() intervalPeriod: number;
@@ -19,12 +17,12 @@ export class DataEmitterComponent implements OnInit {
   minutes: number = 10000;
   subscription$: any;
 
-  constructor(private http: Http) {}
+  constructor(private http: HttpClient) {}
   ngOnInit() {
     this.getData().subscribe(data => {
-        this.data = data;
-        console.log(this.data);
-      });
+      this.data = data;
+      console.log(this.data);
+    });
 
     this.minutes = this.intervalPeriod * 60 * 1000;
     this.subscription$ = interval(this.minutes)
@@ -36,9 +34,7 @@ export class DataEmitterComponent implements OnInit {
   }
 
   getData() {
-    return this.http
-      .get(this.apiUrl)
-      .pipe(map((response: any) => response.json()));
+    return this.http.get(this.apiUrl);
   }
 
   ngOnDestroy() {
